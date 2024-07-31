7b4d43e6bc880598bc9ea78221ee215c2750a9c3
Vinícius Vieira De Andrade Correia <53685550837@info.local>
2024-07-31 17:21:06 -0300

    realizando testes unitario

---
+++ sprint 2/testes_unitários/xUnit_Teste/CalculoUnitTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testes_Unit;
+
+namespace xUnit_Teste
+{
+    public class CalculoUnitTest
+    {
+        [Fact]
+        public void TestarSomaDeDoisNumeros()
+        {
+            var n1 = 3.3;
+            var n2 = 2.2;
+            var valorEsperado = 5.5;
+           
+           
+            var soma = Calculo.Somar(n1, n2);
+
+            Assert.Equal(soma, valorEsperado);
+        }
+
+        [Fact]
+        public void TestarSubtracaoDeDoisNumeros()
+        {
+            var n1 = 5;
+            var n2 = 3;
+            var valorEsperado = 2;
+
+            var subtracao = Calculo.Subtrair(n1, n2);
+
+            Assert.Equal(subtracao, valorEsperado);
+        }
+
+        [Fact]
+        public void TestarMultiplicacaoDeDoisNumeros()
+        {
+            var n1 = 2;
+            var n2 = 2;
+            var valorEsperado = 4;
+
+            var multiplicacao = Calculo.Multiplicar(n1, n2);
+
+            Assert.Equal(multiplicacao, valorEsperado);
+        }
+
+        [Fact]
+        public void TestarDivisaoDeDoisNumeros()
+        {
+            var n1 = 6;
+            var n2 = 2;
+            var valorEsperado = 3;
+
+            var divisao = Calculo.Dividir(n1, n2);
+
+            Assert.Equal(divisao, valorEsperado);
+        }
+    }
+}
